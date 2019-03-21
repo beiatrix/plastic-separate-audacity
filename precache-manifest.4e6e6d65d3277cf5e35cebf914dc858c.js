8809bf873b36906cc99bdffade0bbc657f84d181
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-03-21 00:40:16 -0400

    Updates

---
+++ precache-manifest.4e6e6d65d3277cf5e35cebf914dc858c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e87c50e626c6991d2f1e",
+    "url": "/plastic-separate-audacity/static/js/runtime~main.e6f4c140.js"
+  },
+  {
+    "revision": "8e7e049cb52cc8b07533",
+    "url": "/plastic-separate-audacity/static/js/main.1b34c967.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/plastic-separate-audacity/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "8e7e049cb52cc8b07533",
+    "url": "/plastic-separate-audacity/static/css/main.9af95d79.chunk.css"
+  },
+  {
+    "revision": "a5211eba1245fc0c13b4b7d2a479238c",
+    "url": "/plastic-separate-audacity/index.html"
+  }
+];
